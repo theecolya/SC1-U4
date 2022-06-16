10b124f5ee871914bcda92575c7171b20ef30723
Colya Foxworth <cfox1106@gmail.com>
2022-06-16 14:50:08 -0500

    added id route in projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -12,4 +12,12 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id).then((project) => {
+        project ? res.status(200).json(project) :
+        res.status(404).send(`no project found with id ${req.params.id}`)
+    })
+    .catch(err => console.log(err))
+})
+
 module.exports = router;
