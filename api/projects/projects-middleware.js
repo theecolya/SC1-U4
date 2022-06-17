3e3626522d2585a7f203b79944744ef35f65d4b8
Colya Foxworth <cfox1106@gmail.com>
2022-06-16 20:53:35 -0500

    fixed put

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,8 +1,9 @@
 // add middlewares here related to projects
 const checkBody = (req, res, next) => {
-    if(typeof req.body.name != 'string' || typeof req.body.description === 'string' || typeof req.body.completed != 'boolean') {
+    if(typeof req.body.name != 'string' || typeof req.body.description != 'string' || typeof req.body.completed != 'boolean') {
         res.status(400).send('Ouch: missing info')
-    } else { next() }
+        return
+    } next()
 }
 
 module.exports = {
